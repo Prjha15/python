0914db4aec4aa01a0e0e235705096a838e76809a
Priyanka Jha <priyankajha1516@gmail.com>
2026-03-02 17:23:30 +0530

    Add files via upload

---
+++ geeks_for_geeks(python basic programs)/4.simple interest.py
@@ -0,0 +1,5 @@
+principal=int(input("ENTER THE PRINCIPAL VALUE"))
+rate=int(input("ENTER THE RATE"))
+time=int(input("ENTER THE TOTAL NUMBER OF YEARS"))
+simple_interest=(principal*rate*time)/100
+print("Simple interest is:",simple_interest)
